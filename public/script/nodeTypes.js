58b560f6aa443695fedb61653f2d9d8d1c42b361
SenYanHo <123sendodo@gmail.com>
2022-04-12 00:34:59 +0800

    Updated form to create nodes

--- public/script/nodeTypes.js
+++ public/script/nodeTypes.js
@@ -1,37 +1,31 @@
-let createNodeButton = document.querySelector("#create-node-button");
 let nodeList = document.querySelector(".node-list");
+let nodeSelector = document.querySelector("#node-parent-select");
 
 let nodeData;
 
-function handleLoad() {
-    console.log("Loading Data");
-    fetch("/nodes/list-all", {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST"
-    })
+async function handleLoad() {
+    // Fetch Data from server
+    await fetch("/nodes/list-all")
         .then(res => res.json())
         .then(res => {
             console.log(res);
             nodeData = res;
-
-            nodeList.innerHTML = "";
-
-            for (let node of nodeData) {
-                let nodeElement = document.createElement("li");
-                nodeElement.innerText = node["name"];
-                nodeList.appendChild(nodeElement);
-            }
         });
+
+    // Update HTML
+    nodeList.innerHTML = "";
+    nodeSelector.innerHTML = "";
+
+    for (let node of nodeData) {
+        let nodeElement = document.createElement("li");
+        nodeElement.innerText = node["name"];
+        nodeList.appendChild(nodeElement);
+
+        let listChoice = document.createElement("option");
+        listChoice.innerText = node["name"];
+        listChoice.value = node["name"];
+        nodeSelector.appendChild(listChoice);
+    }
 }
 
-function createNode(event) {
-    event.preventDefault();
-
-
-}
-
-createNodeButton.addEventListener("onclick", (event) => createNode(event));
 window.addEventListener("load", handleLoad);
