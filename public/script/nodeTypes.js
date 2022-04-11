f0fb0d37673e87a58ea61cbea474d0efe9fa5759
SenYanHo <123sendodo@gmail.com>
2022-04-12 01:04:56 +0800

    Updated Nodes List DIsplay
    Node List now shows correctly thanks to recursion

--- public/script/nodeTypes.js
+++ public/script/nodeTypes.js
@@ -3,6 +3,24 @@ let nodeSelector = document.querySelector("#node-parent-select");
 
 let nodeData;
 
+// Update List Recursively
+function generateElement(index) {
+    let data = nodeData[index];
+    let listElement = document.createElement("li");
+    listElement.innerText = data.name;
+    if (data.children.length > 0)
+    {
+        let newList = document.createElement("ul");
+        for (let i = 0; i < data.children.length; i++)
+        {
+            let indexOfChild = nodeData.findIndex(x => x._id === data.children[i])
+            newList.appendChild(generateElement(indexOfChild))
+        }
+        listElement.appendChild(newList);
+    }
+    return listElement;
+}
+
 async function handleLoad() {
     // Fetch Data from server
     await fetch("/nodes/list-all")
@@ -13,19 +31,18 @@ async function handleLoad() {
         });
 
     // Update HTML
-    nodeList.innerHTML = "";
     nodeSelector.innerHTML = "";
 
     for (let node of nodeData) {
-        let nodeElement = document.createElement("li");
-        nodeElement.innerText = node["name"];
-        nodeList.appendChild(nodeElement);
-
         let listChoice = document.createElement("option");
         listChoice.innerText = node["name"];
         listChoice.value = node["name"];
         nodeSelector.appendChild(listChoice);
     }
+
+    let rootIndex = nodeData.findIndex(x => x.name === 'Root');
+    nodeList.innerHTML = "";
+    nodeList.appendChild(generateElement(rootIndex));
 }
 
 window.addEventListener("load", handleLoad);
