0fbb08db8fee1d1c9321b2cc97bfb439af83ed29
SenYanHo <123sendodo@gmail.com>
2022-04-12 01:07:44 +0800

    Updated Timer's Node List

--- public/script/index.js
+++ public/script/index.js
@@ -1,8 +1,24 @@
-let nodeForm = document.querySelector("#nodes");
+let nodeSelector = document.querySelector("#nodes-type");
 let nodeData;
 
-function handleLoad() {
+async function handleLoad() {
+    // Fetch Data from server
+    await fetch("/nodes/list-all")
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            nodeData = res;
+        });
 
+    // Update HTML
+    nodeSelector.innerHTML = "";
+
+    for (let node of nodeData) {
+        let listChoice = document.createElement("option");
+        listChoice.innerText = node["name"];
+        listChoice.value = node["name"];
+        nodeSelector.appendChild(listChoice);
+    }
 }
 
-window.addEventListener("onload", handleLoad);
+window.addEventListener("load", handleLoad);
