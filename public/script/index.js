44e6201fe1ba661cac5e6808f91e65dcf146789b
SenYanHo <123sendodo@gmail.com>
2022-04-12 01:38:41 +0800

    Added frontend timer implementation

--- public/script/index.js
+++ public/script/index.js
@@ -1,15 +1,56 @@
 let nodeSelector = document.querySelector("#nodes-type");
+let timerButton = document.querySelector("#timer-button");
+let timerText = document.querySelector("#timer-text");
 let nodeData;
 
-async function handleLoad() {
-    // Fetch Data from server
-    await fetch("/nodes/list-all")
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            nodeData = res;
-        });
+let timerOn = false;
+let startTime, endTime;
+let timeCnt;
+let intervalCount;
 
+function updateTimer() {
+    timeCnt++;
+    let mins = Math.floor(timeCnt / 60);
+    let secs = timeCnt % 60;
+    mins = mins.toString().padStart(2, "0");
+    secs = secs.toString().padStart(2, "0");
+
+    timerText.innerText = mins + ":" + secs;
+}
+
+function handleTimer(event) {
+    console.log("Timer Button Pressed");
+    event.preventDefault();
+
+    // Deactivate Timer
+    if (timerOn) {
+        timerOn = false;
+        endTime = new Date();
+        timerButton.innerText = "Start Timer";
+        updateNodeSelectors();
+        clearInterval(intervalCount);
+    }
+    // Activate Timer
+    else if (!timerOn) {
+        timeCnt = -1;
+        updateTimer();
+        timerOn = true;
+        startTime = new Date();
+        timerButton.innerText = "Stop Timer";
+        fixNodeSelector();
+        intervalCount = setInterval(updateTimer, 1000);
+    }
+}
+
+function fixNodeSelector() {
+    let listChoice = document.createElement("option");
+    listChoice.innerText = nodeSelector.value;
+    listChoice.value = nodeSelector.value;
+    nodeSelector.innerHTML = "";
+    nodeSelector.appendChild(listChoice);
+}
+
+function updateNodeSelectors() {
     // Update HTML
     nodeSelector.innerHTML = "";
 
@@ -21,4 +62,17 @@ async function handleLoad() {
     }
 }
 
+async function handleLoad() {
+    // Fetch Data from server
+    await fetch("/nodes/list-all")
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            nodeData = res;
+        });
+
+    updateNodeSelectors();
+}
+
+timerButton.addEventListener("click", (e) => handleTimer(e));
 window.addEventListener("load", handleLoad);
