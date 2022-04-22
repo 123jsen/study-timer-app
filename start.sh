c23902fee9e0ede3a6792d5e091916d33b3906bf
SenYanHo <123sendodo@gmail.com>
2022-04-22 13:48:11 +0800

    Replaced css with scss files

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-concurrently "nodemon" "sass --watch public/styles"
+
+# runs nodemon and sass transpiler at the same time
+exec concurrently "nodemon" "sass --watch public/styles"
+
+# remember to ctrl+c to kill the process
+# else, server will still listen on port 3000
